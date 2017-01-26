d6d49b4fd12f528bd5104da8bc9b3bfa8afa8166
Sathish Chinniah <sathish73ec@gmail.com>
2017-01-26 05:55:50 -0800

    init

--- CameraTask/ViewController.swift
+++ CameraTask/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
+    
+     var imagePicker: UIImagePickerController!
 
+    @IBOutlet weak var imagevView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    @IBAction func ChooseImage(_ sender: AnyObject) {
+    
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+       imagePicker.allowsEditing = true
+       imagevView.contentMode = .scaleAspectFill
 
+        
+        present(imagePicker, animated: true, completion: nil)
+    
+    }
+  
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        imagevView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        imagevView.image = info[UIImagePickerControllerEditedImage] as! UIImage?
+        
+      
+    }
+    
+    
 
 }
 
